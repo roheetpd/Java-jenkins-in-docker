85876556b2d260c29b336d16a96032fee14ade25
Rohit Digraskar <roheetpd@gmail.com>
2022-01-09 10:47:55 +0530

    commit 1st

---
+++ Java-jenkins-in-docker/src/main/java/org/example/Java_jenkins_in_docker/App.java
@@ -0,0 +1,20 @@
+package org.example.Java_jenkins_in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    		System.out.println(checkIfInputIsAnEvenNumber(122));
+    		// Testing in the main method
+    		}
+
+    		public static boolean checkIfInputIsAnEvenNumber(int
+    		number){
+    		return number % 2 == 0;
+    		}
+    		}
+   
